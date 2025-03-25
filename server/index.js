029ddbe89f241834c9f8c36a39a53ac20e0cb05d
Muhammad Aleem <aleemzafar035@gmail.com>
2025-03-25 15:06:00 +0500

    added channel namess

--- server/index.js
+++ server/index.js
@@ -15,7 +15,10 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://timezonefirstapi.vercel.app/", // Remove the trailing slash
+  methods: ["POST", "GET"]// Use 'methods' instead of 'method'
+}));
  
  
 app.use(express.json());
